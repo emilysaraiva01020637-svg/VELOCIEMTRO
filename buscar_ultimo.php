096681635264cb027993fd48384cbfa378651935
emily <emily.saraiva01020637@sesisenaipr.org.br>
2026-06-03 15:41:36 -0300

    Update buscar_ultimo.php

--- buscar_ultimo.php
+++ buscar_ultimo.php
@@ -1,12 +1,31 @@
 <?php
+
+$conn = new mysqli(
+    "localhost",
+    "root",
+    "",
+    "telemetria"
+);
+
+$sql = "
+    SELECT velocidade, rpm
+    FROM leituras
+    ORDER BY id DESC
+    LIMIT 1
+";
+
 $result = $conn->query($sql);
+
 if ($result->num_rows > 0) {
-$row = $result->fetch_assoc();
-echo json_encode([
-"exemplo" => $row["exemplo"],
-"exemplo" => $row["exemplo"],
-"exemplo" => $row["exemplo"]
-]);
+
+    $row = $result->fetch_assoc();
+
+    echo json_encode([
+        "velocidade" => $row["velocidade"],
+        "rpm" => $row["rpm"]
+    ]);
 }
+
 $conn->close();
+
 ?>
