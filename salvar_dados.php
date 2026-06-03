0985f231b229b4e6a1b21e69a87c9a1cdb9305e4
emily <emily.saraiva01020637@sesisenaipr.org.br>
2026-06-03 15:40:46 -0300

    Update salvar_dados.php

--- salvar_dados.php
+++ salvar_dados.php
@@ -1,6 +1,35 @@
 <?php
- if (isset($_POST['exemplo']) && isset($_POST['exemplo'])) {
-$vel = $_POST['exemplo'];
+
+$conn = new mysqli(
+    "localhost",
+    "root",
+    "",
+    "telemetria"
+);
+
+if (
+    isset($_POST['velocidade']) &&
+    isset($_POST['rpm'])
+) {
+
+    $velocidade = floatval($_POST['velocidade']);
+    $rpm = floatval($_POST['rpm']);
+
+    $stmt = $conn->prepare(
+        "INSERT INTO leituras (velocidade, rpm)
+         VALUES (?, ?)"
+    );
+
+    $stmt->bind_param(
+        "dd",
+        $velocidade,
+        $rpm
+    );
+
+    $stmt->execute();
+    $stmt->close();
+}
+
 $conn->close();
- }
+
 ?>
